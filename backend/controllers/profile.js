b229d340e10e5bab8f360983203e758f93825565
abhyudaya29 <abhyudayadev123@gmail.com>
2024-04-01 18:01:58 +0530

    update display controller

--- backend/controllers/profile.js
+++ backend/controllers/profile.js
@@ -106,3 +106,62 @@ exports.getAllUserDetails=async(req,res)=>{
         })
     }
 }
+exports.updateDisplayPicture=async(req,res)=>{
+    try {
+        const displayImage=req.file.path;
+        const userId=req.user.id
+        const image=await uploadImageToCloudinary(
+            displayImage,
+            process.env.FOLDER_NAME,
+            1000,
+            1000
+        )
+        console.log(image,">>>image");
+
+        const updateProfile=await User.findByIdAndUpdate(
+            {id:userId},
+            {image:image.secure_url},
+            {new:true},);
+        return res.status(200).json({
+            success:true,
+            message: `Image Updated successfully`,
+            data: updateProfile,
+    
+        })
+        
+
+        
+    } catch (error) {
+        console.log(error,"error occured during update diplay picture")
+        return res.status(500).json({
+            success: false,
+            message: error.message}
+    )}
+}
+
+exports.getEnrolledCourses=async(req,res)=>{
+    try {
+        const userId=req.user.id
+        const userDetails=await User.findOne({
+            _id:userId, 
+        }).populate("courses").exec();
+
+        if (!userDetails) {
+            return res.status(400).json({
+              success: false,
+              message: `Could not find user with id: ${userDetails}`,
+            })
+          }
+          return res.status(200).json({
+            success: true,
+            data: userDetails.courses,
+          })
+        
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+          })
+        
+    }
+}
