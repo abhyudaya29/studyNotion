3d2d06e00eeaf2d43d79dbf9cf1ca0778915cd54
abhyudaya29 <abhyudayadev123@gmail.com>
2024-04-09 23:43:03 +0530

    router setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 
 
 import './App.css'
-
+import{Route,Routes} from "react-router-dom"
+import Home from './pages/home'
 function App() {
   
 
   return (
     <>
-    <h1>Study notion</h1>
+    <div className='w-screen min-h-screen bg-richblack-900 flex flex-col font-inter'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+
+    </div>
     </>
   )
 }
