541a418662d0089535559a12bf765201cabce4ae
abhyudaya29 <abhyudayadev123@gmail.com>
2024-04-17 00:00:19 +0530

    App file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,19 @@
 import './App.css'
 import{Route,Routes} from "react-router-dom"
 import Home from './pages/home'
+import SignUp from './pages/signup'
+import Login from './pages/login'
 function App() {
   
 
   return (
     <>
-    <div className='w-screen min-h-screen bg-richblack-900 flex flex-col font-inter'>
+    <div className='w-screen min-h-screen bg-richblack-900 flex flex-col font-inter '>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/login' element={<Login/>}/>
+        
       </Routes>
 
     </div>
