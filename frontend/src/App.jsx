5e07af82ff415d2ea554e1c92c3658d366ba5405
abhyudaya29 <abhyudayadev123@gmail.com>
2024-04-17 16:05:06 +0530

    app jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,14 @@ import{Route,Routes} from "react-router-dom"
 import Home from './pages/home'
 import SignUp from './pages/signup'
 import Login from './pages/login'
+import NavBar from './components/common/NavBar'
 function App() {
   
 
   return (
     <>
     <div className='w-screen min-h-screen bg-richblack-900 flex flex-col font-inter '>
+      <NavBar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/signup' element={<SignUp/>}/>
