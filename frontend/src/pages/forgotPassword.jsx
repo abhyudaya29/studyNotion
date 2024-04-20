63e1d26f4428bdabac2fb9f7d47d1a9ae6bee3bf
abhyudaya29 <abhyudayadev123@gmail.com>
2024-04-20 15:03:38 +0530

    forgot password

---
+++ frontend/src/pages/forgotPassword.jsx
@@ -0,0 +1,76 @@
+/* eslint-disable no-undef */
+import { useDispatch, useSelector } from "react-redux"
+import { useState } from "react"
+import { Link } from "react-router-dom";
+import { getPasswordResetToken } from "../services/operations/auth";
+const ForgotPassword = () => {
+    const[emailSent,setEmailSent]=useState(false);
+    const[email,setEmail]=useState("");
+    const dispatch=useDispatch();
+    const{loading}=useSelector((state)=>state.auth)
+    const handleOnSubmit=(e)=>{
+        e.preventDefault();
+        dispatch(getPasswordResetToken)
+        
+
+    }
+  return (
+    <>
+    <div className="text-white flex justify-center items-center">
+        {
+            loading?(
+                <>
+                <div>Loading....</div>
+                </>
+            ):(
+                <>
+                <div>
+                <h1>{
+                    !emailSent?"Reset Your Password":"Check Your Emal"
+                    }
+                </h1>
+                <p>
+                    {
+                        !emailSent?"Have no fear. We’ll email you instructions to reset your password. If you dont have access to your email we can try account recovery"
+                        :
+                        `We have sent the reset email to ${email}`
+                    }
+                </p>
+                <form onSubmit={handleOnSubmit}>
+                    {
+                        !emailSent &&(
+                            <label>
+                                <p>Email Address:</p>
+                                <input type="email"
+                                required
+                                name="email"
+                                value={email}
+                                onChange={(e)=>setEmail(e.target.value)}
+                                placeholder="enter Your email Address"
+                                 />
+                            </label>
+                        )
+                    }
+                    <button type="submit">
+                        {
+                            !emailSent?"ResetPassword":"Resend Email"
+                        }
+                    </button>
+                </form>
+                <div>
+                    <Link to="/login">
+                        <p>Back to Login</p>
+                    </Link>
+                </div>
+                </div>
+
+                </>
+            )
+
+        }
+    </div>
+    </>
+  )
+}
+
+export default ForgotPassword
