64215f21f139d984171cc2476a5d9e18ea6fb555
abhyudaya29 <abhyudayadev123@gmail.com>
2024-04-21 14:58:21 +0530

    profile slice

--- frontend/src/slices/profileSlice.js
+++ frontend/src/slices/profileSlice.js
@@ -12,5 +12,5 @@ const profileSlice=createSlice({
         }
     }
 })
-export const {setToken}=profileSlice.actions;
+export const {setUser}=profileSlice.actions;
 export default profileSlice.reducer
