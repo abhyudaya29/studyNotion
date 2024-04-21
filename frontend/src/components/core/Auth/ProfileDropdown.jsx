7c4ef61a70601a59fc16854ddaeb8c2af675cb46
abhyudaya29 <abhyudayadev123@gmail.com>
2024-04-21 14:57:43 +0530

    profile dropdown

--- frontend/src/components/core/Auth/ProfileDropdown.jsx
+++ frontend/src/components/core/Auth/ProfileDropdown.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const ProfileDropdown = () => {
+  const {user}=useSelector((state)=>state.profile);
+  console.log(user,">>image")
+
   return (
-    <div>ProfileDropdown</div>
+    <>
+    <div>
+      {/* image */}
+      <img
+          src={user?.image}
+          alt={`profile-${user?.firstName}`}
+          className="aspect-square w-[30px] rounded-full object-cover"
+        />
+    </div>
+    </>
   )
 }
 
 export default ProfileDropdown
+
+
