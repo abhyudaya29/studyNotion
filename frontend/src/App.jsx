116cc08da1606f8a23ec9c0ccce9ffca3725d33d
abhyudaya29 <abhyudayadev123@gmail.com>
2024-04-25 01:18:23 +0530

    app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,17 +8,19 @@ import Login from './pages/login'
 import NavBar from './components/common/NavBar'
 import VerifyEmail from './components/core/Auth/VerifyEmail'
 import ForgotPassword from './pages/forgotPassword'
+import AboutUs from './pages/aboutUs'
 function App() {
   
 
   return (
     <>
-    <div className='w-screen min-h-screen bg-richblack-900 flex flex-col font-inter '>
+    <div className='w-screen min-h-screen  bg-richblack-900 flex flex-col font-inter '>
       <NavBar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/signup' element={<SignUp/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/about' element={<AboutUs/>}/>
         <Route path='/forgot-password' element={<ForgotPassword/>}/>
 
         <Route path="/verify-email" element={<VerifyEmail/>}/>
