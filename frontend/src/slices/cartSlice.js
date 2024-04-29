320c429ba7debaf968a73a7e898f367d020a39ef
abhyudaya29 <abhyudayadev123@gmail.com>
2024-04-29 16:16:30 +0530

    cart slice

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { Toaster } from "react-hot-toast";
+import{toast} from "react-hot-toast"
 
 const initialState={
     totalItems:localStorage.getItem("totalItems")? JSON.parse(localStorage.getItem("totalItems")):0
