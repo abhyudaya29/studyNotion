3544058f0c78f4c1670ca1f26849a0dcfdc4012f
abhyudaya29 <abhyudayadev123@gmail.com>
2024-05-03 14:42:37 +0530

    course slice

---
+++ frontend/src/slices/errorSlice.js
@@ -0,0 +1,20 @@
+// import {createSlice} from "@reduxjs/toolkit";
+
+// const initialState={
+//     error:null
+// }
+
+
+// const errorSlice=createSlice({
+//     name:"error",
+//     initialState:initialState,
+//     reducers:{
+//         addError:(state,action)=>{
+//             state.error=action.payload
+//         }
+//     }
+
+// })
+
+// export const {addError} =errorSlice.actions
+// export default errorSlice.reducer
